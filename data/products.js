34d180002af052ace7b4dd49de321affdb1b1996
Aliyu Tunde <aliumuhammad2000@gmail.com>
2026-02-18 03:28:09 -0800

    added eventlisteners and put products on the page using the dom

---
+++ data/products.js
@@ -0,0 +1,31 @@
+export const products = [{
+    id: 'pd1',
+    image: '/images/top6.webp',
+    category: 'Top',
+    name: 'crochet top',
+    price: '15000'
+}, {
+    id: 'pd2',
+    image: '/images/pant3.webp',
+    category: 'trousers',
+    name: 'bell buttom pants',
+    price: '20000'
+}, {
+    id: 'pd3',
+    image: '/images/hot1.jpg',
+    category: 'Underwears',
+    name: 'Red sexy Lingerie',
+    price: '10000'
+}, {
+    id: 'pd4',
+    image: '/images/nice1.jpg',
+    category: 'top',
+    name: 'body hug top',
+    price: '9000'
+}, {
+    id: 'pd5',
+    image: '/images/hood2.webp',
+    category: 'hood',
+    name: 'men hoody',
+    price: '25000'
+}]
---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+import { products } from "../data/products.js";
+
+const navBar = document.querySelector('.nav-bar');
+const menuBar = document.querySelector('.menu-bar');
+const menuIcon = document.querySelector('.menu-icon');
+const menuClose = document.querySelector('.menu-close');
+const cartIcon = document.querySelector('.cart-icon');
+const cartPage = document.querySelector('.cart-page');
+const cartClose = document.querySelector('.cart-close');
+
+
+menuBar.addEventListener('click', () => {
+    navBar.classList.toggle('hidden');
+    menuIcon.classList.toggle('hidden');
+    menuClose.classList.toggle('hidden');
+});
+cartIcon.addEventListener('click', () => {
+    cartPage.classList.toggle('hidden');
+});
+cartClose.addEventListener('click', () => {
+    cartPage.classList.add('hidden');
+});
+
+let productsHTML = '';
+products.forEach((product) => {
+    productsHTML += `
+    <div class="h-fit relative">
+        <img class="w-full h-40 object-cover transition-transform hover:scale-110 cursor-pointer" src="${product.image}" alt="Lingerie">
+        <div class="font-medium leading-snug capitalize py-2">
+            <span class="text-xs">${product.category}</span>
+            <p class="text-sm">${product.name}</p>
+        </div>
+        <span class="font-bold">&#8358;${product.price}</span>
+        <button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-blue-600 absolute right-0 bottom-0.5 cursor-pointer">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+            </svg>
+        </button>
+    </div>
+    `
+});
+
+document.getElementById('js-products').innerHTML = productsHTML;
