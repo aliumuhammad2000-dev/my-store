ffcd956055f065dd61417fc99e293ec6e1e63355
Aliyu Tunde <aliumuhammad2000@gmail.com>
2026-03-02 03:00:18 -0800

    added products photo and to product.js

--- data/products.js
+++ data/products.js
@@ -13,7 +13,7 @@ export const products = [{
 }, {
     id: 'pd3',
     image: '/images/hot1.jpg',
-    category: 'Underwears',
+    category: 'Bikini',
     name: 'Red sexy Lingerie',
     price: '10000'
 }, {
@@ -26,6 +26,78 @@ export const products = [{
     id: 'pd5',
     image: '/images/hood2.webp',
     category: 'hood',
-    name: 'men hoody',
+    name: 'men hoody overall',
+    price: '25000'
+}, {
+    id: 'pd6',
+    image: '/images/hot2.jpg',
+    category: 'Bikini',
+    name: 'black sexy lingerie',
+    price: '25000'
+}, {
+    id: 'pd7',
+    image: '/images/hot4.jpg',
+    category: 'bikini',
+    name: 'Pinky lace',
+    price: '25000'
+}, {
+    id: 'pd8',
+    image: '/images/hot5.jpg',
+    category: 'bikini',
+    name: 'red hot sauce lingerie',
+    price: '25000'
+}, {
+    id: 'pd9',
+    image: '/images/hood4.webp',
+    category: 'men wears',
+    name: 'hoody',
+    price: '25000'
+}, {
+    id: 'pd10',
+    image: '/images/hood5.webp',
+    category: 'men wears',
+    name: 'hoody',
+    price: '25000'
+}, {
+    id: 'pd11',
+    image: '/images/hood6.webp',
+    category: 'men wears',
+    name: 'hoody',
+    price: '25000'
+}, {
+    id: 'pd12',
+    image: '/images/hood7.webp',
+    category: 'men wears',
+    name: 'hoody',
+    price: '25000'
+}, {
+    id: 'pd13',
+    image: '/images/hood8.webp',
+    category: 'men wears',
+    name: 'hoody',
+    price: '25000'
+}, {
+    id: 'pd14',
+    image: '/images/hood9.webp',
+    category: 'men wears',
+    name: 'hoody',
+    price: '25000'
+}, {
+    id: 'pd15',
+    image: '/images/hood10.webp',
+    category: 'men wears',
+    name: 'hoody',
+    price: '25000'
+}, {
+    id: 'pd16',
+    image: '/images/hood3.webp',
+    category: 'men wears',
+    name: 'hoody',
+    price: '25000'
+}, {
+    id: 'pd17',
+    image: '/images/hood1.webp',
+    category: 'men wears',
+    name: 'jacket',
     price: '25000'
 }]
